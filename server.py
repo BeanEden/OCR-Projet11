f486d209f163067932a6df8b4ea4f5a72ac9b797
JC <bean3den@gmail.com>
2022-07-07 11:03:49 +0200

    Commit before simplifying update and load DB

--- server.py
+++ server.py
@@ -1,25 +1,71 @@
 import json
 from flask import Flask,render_template,request,redirect,flash,url_for
 
+club_file ='clubs.json'
+competition_file = 'competitions.json'
+
+app = Flask(__name__)
+app.secret_key = 'something_special'
 
 def loadClubs():
-    with open('clubs.json') as c:
+    with open(club_file) as c:
          listOfClubs = json.load(c)['clubs']
          return listOfClubs
 
 
 def loadCompetitions():
-    with open('competitions.json') as comps:
+    with open(competition_file) as comps:
          listOfCompetitions = json.load(comps)['competitions']
          return listOfCompetitions
-
-
-app = Flask(__name__)
-app.secret_key = 'something_special'
 
 competitions = loadCompetitions()
 clubs = loadClubs()
 
+def updateClubsDB():
+    with open(club_file, "w") as c:
+        data = {'clubs': clubs}
+        json.dump(data, c)
+
+def updateCompetitionDB():
+    with open('competitions.json', "w") as cr:
+        data = {'competitions': competitions}
+        json.dump(data, cr)
+
+
+def loadDB(db_file, name):
+    with open(db_file) as c:
+         db_list = json.load(c)[name]
+         return db_list
+
+
+def updateDB(db_file, data_dict):
+    with open(db_file, "w") as c:
+        json.dump(data_dict, c)
+
+
+def loadPlacesAlreadyBooked(competition, club):
+    if len(competition['clubsParticipating']) > 0:
+        for i in competition['clubsParticipating']:
+            if club['name'] == i['club']:
+                return int(i['placesBooked'])
+    else:
+        return 0
+
+
+def updatePlacesBookedOrCreate(competition, club, places):
+    if len(competition['clubsParticipating']) > 0:
+        count=0
+        for i in competition['clubsParticipating']:
+            if club['name'] == i['club']:
+                i['placesBooked'] = places
+                count += 1
+        if count == 0:
+            competition["clubsParticipating"].append(
+                {'club': club['name'], 'placesBooked': places})
+        return competition
+    else:
+        competition["clubsParticipating"].append({'club' : club['name'], 'placesBooked' : places})
+        return competition
 
 @app.route('/')
 def index(error_message="False"):
@@ -50,19 +96,30 @@ def book(competition, club):
 def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
+    placesAlreadyBooked = loadPlacesAlreadyBooked(competition, club)
+    print(placesAlreadyBooked, type(placesAlreadyBooked))
     placesRequired = int(request.form['places'])
+    totalPlacesBooked = 0+placesRequired
     if placesRequired > int(club['points']):
         error_message = "You don't have enough points to make this reservation"
         return render_template('booking.html', club=club, competition=competition, error_message=error_message)
+    elif totalPlacesBooked > 12:
+        error_message = "You can't book more than 12 places for an event"
+        return render_template('booking.html', club=club, competition=competition,
+                               error_message=error_message)
     else:
         competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+        competition = updatePlacesBookedOrCreate(competition, club, totalPlacesBooked)
         club['points'] = int(club['points'])-placesRequired
-        with open('clubs.json',"w") as c:
-            data = {'clubs': clubs}
-            json.dump(data, c)
-        with open('competitions.json', "w") as cr:
-            data = {'competitions': competitions}
-            json.dump(data, cr)
+        # with open('clubs.json',"w") as c:
+        #     data = {'clubs': clubs}
+        #     json.dump(data, c)
+        updateClubsDB()
+        club_db = updateDB(club_file, {"clubs":clubs})
+        competition_db = updateDB(competition_file, {"competitions":competitions})
+        # with open('competitions.json', "w") as cr:
+        #     data = {'competitions': competitions}
+        #     json.dump(data, cr)
         flash('Great-booking complete!')
         return render_template('welcome.html', club=club, competitions=competitions)
 
