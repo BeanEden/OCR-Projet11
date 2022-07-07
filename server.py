066b4c0ab750b2ca52201d7f8f9e870681c2532f
JC <bean3den@gmail.com>
2022-07-07 11:55:54 +0200

    simplified update and load DB Places already booked successfully managed Proper management of competitions places

--- server.py
+++ server.py
@@ -1,36 +1,22 @@
 import json
 from flask import Flask,render_template,request,redirect,flash,url_for
 
-club_file ='clubs.json'
+
 competition_file = 'competitions.json'
+club_file ='clubs.json'
+
 
 app = Flask(__name__)
 app.secret_key = 'something_special'
 
-def loadClubs():
-    with open(club_file) as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
-
-
-def loadCompetitions():
-    with open(competition_file) as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
-
-competitions = loadCompetitions()
-clubs = loadClubs()
-
-def updateClubsDB():
-    with open(club_file, "w") as c:
-        data = {'clubs': clubs}
-        json.dump(data, c)
-
-def updateCompetitionDB():
-    with open('competitions.json', "w") as cr:
-        data = {'competitions': competitions}
-        json.dump(data, cr)
-
+# def loadClubs():
+#     with open(club_file) as c:
+#          listOfClubs = json.load(c)['clubs']
+#          return listOfClubs
+# def loadCompetitions():
+#     with open(competition_file) as comps:
+#          listOfCompetitions = json.load(comps)['competitions']
+#          return listOfCompetitions
 
 def loadDB(db_file, name):
     with open(db_file) as c:
@@ -38,9 +24,24 @@ def loadDB(db_file, name):
          return db_list
 
 
+competitions = loadDB(competition_file, 'competitions')
+clubs = loadDB(club_file, 'clubs')
+
+# def updateClubsDB():
+#     with open(club_file, "w") as c:
+#         data = {'clubs': clubs}
+#         json.dump(data, c)
+#
+# def updateCompetitionDB():
+#     with open('competitions.json', "w") as cr:
+#         data = {'competitions': competitions}
+#         json.dump(data, cr)
+
+
 def updateDB(db_file, data_dict):
     with open(db_file, "w") as c:
         json.dump(data_dict, c)
+
 
 
 def loadPlacesAlreadyBooked(competition, club):
@@ -64,8 +65,9 @@ def updatePlacesBookedOrCreate(competition, club, places):
                 {'club': club['name'], 'placesBooked': places})
         return competition
     else:
-        competition["clubsParticipating"].append({'club' : club['name'], 'placesBooked' : places})
+        competition["clubsParticipating"].append({'club': club['name'], 'placesBooked': places})
         return competition
+
 
 @app.route('/')
 def index(error_message="False"):
@@ -86,7 +88,8 @@ def book(competition, club):
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        placesAlreadyBooked = loadPlacesAlreadyBooked(foundCompetition, foundClub)
+        return render_template('booking.html',club=foundClub,competition=foundCompetition, placesAlreadyBooked=placesAlreadyBooked)
     else:
         flash("Something went wrong-please try again")
         return render_template('welcome.html', club=club, competitions=competitions)
@@ -97,29 +100,21 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesAlreadyBooked = loadPlacesAlreadyBooked(competition, club)
-    print(placesAlreadyBooked, type(placesAlreadyBooked))
     placesRequired = int(request.form['places'])
-    totalPlacesBooked = 0+placesRequired
+    totalPlacesBooked = placesAlreadyBooked+placesRequired
     if placesRequired > int(club['points']):
         error_message = "You don't have enough points to make this reservation"
         return render_template('booking.html', club=club, competition=competition, error_message=error_message)
     elif totalPlacesBooked > 12:
         error_message = "You can't book more than 12 places for an event"
-        return render_template('booking.html', club=club, competition=competition,
+        return render_template('booking.html', club=club, competition=competition, placesAlreadyBooked=placesAlreadyBooked,
                                error_message=error_message)
     else:
         competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
         competition = updatePlacesBookedOrCreate(competition, club, totalPlacesBooked)
         club['points'] = int(club['points'])-placesRequired
-        # with open('clubs.json',"w") as c:
-        #     data = {'clubs': clubs}
-        #     json.dump(data, c)
-        updateClubsDB()
         club_db = updateDB(club_file, {"clubs":clubs})
         competition_db = updateDB(competition_file, {"competitions":competitions})
-        # with open('competitions.json', "w") as cr:
-        #     data = {'competitions': competitions}
-        #     json.dump(data, cr)
         flash('Great-booking complete!')
         return render_template('welcome.html', club=club, competitions=competitions)
 
